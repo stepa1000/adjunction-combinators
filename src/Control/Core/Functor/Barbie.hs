a92a6f4bab1bbcd2fb5e38954324bb42370b779d
stepa <stepa100023@gmail.com>
2024-01-24 14:13:18 +0600

    functor-combinators   > /tmp/stack-0fe9998779b9797e/functor-combinators-0.4.1.2/src/Data/Functor/Contravariant/Divise.hs:29:1: error: functor-combinators   >     Could not find module ‘Control.Monad.Trans.Error’ functor-combinators   >     Perhaps you meant functor-combinators   >       Control.Monad.Trans.Free (from free-5.2) functor-combinators   >       Control.Monad.Trans.Iter (from free-5.2) functor-combinators   >       Control.Monad.Trans.Writer (from transformers-0.6.1.0) functor-combinators   >     Use -v (or `:set -v` in ghci) to see a list of the files searched for. functor-combinators   >    | functor-combinators   > 29 | import           Control.Monad.Trans.Error functor-combinators   >    | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ functor-combinators   > functor-combinators   > /tmp/stack-0fe9998779b9797e/functor-combinators-0.4.1.2/src/Data/Functor/Contravariant/Divise.hs:32:1: error: functor-combinators   >     Could not find module ‘Control.Monad.Trans.List’ functor-combinators   >     Perhaps you meant functor-combinators   >       Control.Monad.Trans.Cont (from transformers-0.6.1.0) functor-combinators   >       Control.Monad.Trans.RWS (from transformers-0.6.1.0) functor-combinators   >       Control.Monad.Trans.Free (from free-5.2) functor-combinators   >     Use -v (or `:set -v` in ghci) to see a list of the files searched for. functor-combinators   >    | functor-combinators   > 32 | import           Control.Monad.Trans.List

--- src/Control/Core/Functor/Barbie.hs
+++ src/Control/Core/Functor/Barbie.hs
@@ -26,7 +26,7 @@ import Data.Proxy
 -}
 
 import Barbies
-import Control.Applicative.ListF
+-- import Control.Applicative.ListF
 import Control.Arrow
 import Control.Comonad
 import Control.Comonad.Trans.Adjoint as W
@@ -69,7 +69,7 @@ bCompAdjW ::
   b (W.AdjointT f2 g2 w) ->
   b (W.AdjointT (f2 :.: f1) (g1 :.: g2) w)
 bCompAdjW fa f b1 b2 = bmap (\(Pair adj1 adj2) -> extend f (compAdjComonad fa adj1 adj2)) $ bprod b1 b2
-
+{-
 bCombAdjW ::
   (ApplicativeB b, Adjunction f1 g1, Adjunction f2 g2, Comonad w) =>
   (forall a. W.AdjointT (f1 :+: f2) (g1 :*: g2) w (Either a a) -> a) ->
@@ -77,3 +77,4 @@ bCombAdjW ::
   b (W.AdjointT f2 g2 w) ->
   b (ListF (W.AdjointT (f1 :+: f2) (g1 :*: g2) w))
 bCombAdjW f b1 b2 = bmap (\(Pair adj1 adj2) -> ListF $ fmap (extend f) (adj1 @+* adj2)) $ bprod b1 b2
+-}
